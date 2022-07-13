1d8a0d36d1c1ac93542a57d80f99ad15f708a2ef
vitche <developers@vitche.com>
2022-07-14 00:03:44 +0300

    Update calls.py

--- onion_redis/calls.py
+++ onion_redis/calls.py
@@ -10,5 +10,9 @@ class OnionRedisHTTPCall:
     def consume(self, namespace, class_name, method_name, arguments):
         uri = "%s/%s/%s/%s" % (self.uri, namespace, class_name, method_name)
         response = requests.post(uri, json.dumps(arguments), headers={"Content-type": "application/json"})
-        response = json.loads(response.text)
+        response = response.text
+        response = json.loads(response)
+        # A tricky way to parse the JSON string encoded twice in Onion Redis
+        if isinstance(response, str):
+          response = json.loads(response)
         return response
